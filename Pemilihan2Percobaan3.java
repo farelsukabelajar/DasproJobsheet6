72ae6a2ee1c21e3ef9df5f6d01ad4542c4d8ef05
farelsukabelajar <keyfarel645@gmail.com>
2023-10-06 07:07:35 +0700

    Tugas 6

---
+++ Pemilihan2Percobaan3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan3 {
+    public static void main(String[] args) {
+
+        Scanner input15 = new Scanner(System.in);
+
+        String kategori;
+        int penghasilan, gajiBersih;
+        double pajak = 0;
+
+        System.out.print("Masukan Kategori : ");
+        kategori = input15.nextLine();
+        System.out.print("Masukan Besarnya Penghasilan : ");
+        penghasilan = input15.nextInt();
+
+if(kategori.equalsIgnnoreCase(anotherString:"pekerja" )){
+    if(penghasilan <= 2000000)
+    pajak = 0.1;
+    else if(penghasilan <= 3000000)
+    pajak = 0.15;
+    else
+    pajak = 0.2;
+}
+else if(kategori.equalsIgnoreCase(anotherString:"pembisnis")){
+if(penghasilan <= 2500000)
+pajak = 0.15;
+else if(penghasilan <= 3500000)
+pajak = 0.2;
+else
+pajak = 0.25;
+
+gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+System.out.print("Penghasilan Bersih : " + gajiBersih);
+
+}
+else{
+    System.out.print("Masukan Salah");
+}
+
+
+
+    }
+}
